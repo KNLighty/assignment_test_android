654655831013858c189bb5d968a4e0cb35a43644
Kirill_Zhirikhin <kirill.ver1@gmail.com>
2022-01-28 18:36:01 +0300

    refactoring

--- app/src/main/java/com/example/testassignmentandroid/MainActivity.java
+++ app/src/main/java/com/example/testassignmentandroid/MainActivity.java
@@ -7,15 +7,12 @@ import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -28,21 +25,18 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private ArrayList<CurrencyRateModel> listOfCurrency = new ArrayList<>();
-    private String URI = "https://www.cbr-xml-daily.ru/daily_json.js";
-    private TextView tv;
-    private String myResponse;
-    private ListView lv;
-    ArrayList<HashMap<String,String>> arrayList;
+    private static final String URI = "https://www.cbr-xml-daily.ru/daily_json.js";
+    private String response;
+    private ListView listView;
+    ArrayList<HashMap<String, String>> currencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        arrayList = new ArrayList<>();
-        listOfCurrency  = new ArrayList<>();
-        lv = (ListView) findViewById(R.id.listview);
+        currencies = new ArrayList<>();
+        listView = findViewById(R.id.listview);
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(URI)
@@ -50,19 +44,19 @@ public class MainActivity extends AppCompatActivity {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
+                Log.e(TAG, e.getMessage());
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 if (response.isSuccessful()) {
-                    myResponse = response.body().string();
+                    MainActivity.this.response = response.body().string();
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            HashMap<String,String> data;
+                            HashMap<String, String> data;
                             try {
-                                JSONObject reader = new JSONObject(myResponse);
+                                JSONObject reader = new JSONObject(MainActivity.this.response);
                                 JSONObject rbcGETRates = new JSONObject(reader.toString());
                                 JSONObject jSonValute = rbcGETRates.getJSONObject("Valute");
                                 Iterator<String> arrayKey = jSonValute.keys();
@@ -70,33 +64,38 @@ public class MainActivity extends AppCompatActivity {
                                 while (arrayKey.hasNext()) {
                                     String key = arrayKey.next();
                                     JSONObject jSonItem = jSonValute.getJSONObject(key);
-                                    data = new HashMap<>();
-
-                                    data.put("id", jSonItem.getString("ID"));
-                                    data.put("numcode", jSonItem.getString("NumCode"));
-                                    data.put("charcode", jSonItem.getString("CharCode"));
-                                    data.put("nominal", jSonItem.getString("Nominal"));
-                                    data.put("name", jSonItem.getString("Name"));
-                                    data.put("value", jSonItem.getString("Value"));
-                                    data.put("previous", jSonItem.getString("Previous"));
-
-                                    arrayList.add(data);
+                                    data = formDataMap(jSonItem);
+                                    currencies.add(data);
 
                                     ListAdapter adapter = new SimpleAdapter(
                                             MainActivity.this,
-                                            arrayList,
+                                            currencies,
                                             R.layout.listview_layout,
                                             new String[]{"id", "numcode", "charcode", "nominal", "name", "value", "previous"},
                                             new int[]{R.id.id, R.id.numcode, R.id.charcode, R.id.nominal, R.id.name, R.id.value, R.id.previous});
-                                    lv.setAdapter(adapter);
+                                    listView.setAdapter(adapter);
                                 }
                             } catch (JSONException e) {
-                                Log.e(TAG, "ОШИБКА");
+                                Log.e(TAG, e.getMessage());
                             }
                         }
                     });
                 }
             }
         });
+    }
+
+    private HashMap<String, String> formDataMap(JSONObject jSonItem) throws JSONException {
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("id", jSonItem.getString("ID"));
+        data.put("numcode", jSonItem.getString("NumCode"));
+        data.put("charcode", jSonItem.getString("CharCode"));
+        data.put("nominal", jSonItem.getString("Nominal"));
+        data.put("name", jSonItem.getString("Name"));
+        data.put("value", jSonItem.getString("Value"));
+        data.put("previous", jSonItem.getString("Previous"));
+
+        return data;
     }
 }
